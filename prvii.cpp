26d928d9e4d91738acd3e61a72bdc80733956efc
mmklub123 <milan@mmklub.rs>
2020-03-23 09:20:50 +0100

    Add files via upload

---
+++ prvii.cpp
@@ -0,0 +1,21 @@
+ 
+#include <iostream>
+#include <fstream>
+#include <string>
+//Milan Milivojevic pripremio zadatak
+using namespace std;
+ int main()
+ {
+ 	int i=0;
+ 	int k=10;
+ 	while(i<k){
+	   cout<<"*";
+ 	   ++i;
+     	 }
+ 	return 0;
+ 	
+	 }
+	 // testirati promeniti podatke 
+	 //postaviti pitanja ako nesto nije jasno
+	 //objasniti za i=0, k=-2; i=0, k=-2;
+	 //dobiti isti rezultat koriscenjem a)i--;b)k--
